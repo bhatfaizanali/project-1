4c45eb697de27b7b12d66cd76ad0251396d721ea
Faizan Bhat <36982735+bhatfaizanali@users.noreply.github.com>
2020-02-19 15:35:29 +0530

    Update sslscan.sh

--- scripts/sslscan.sh
+++ scripts/sslscan.sh
@@ -3,3 +3,4 @@
 
 cat ../database/$2/$1/subdomains/liveSubdomainsWithoutHandlers.txt | while read domain; do sslscan $domain | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g' | tee ../database/$2/$1/scandata/sslscan/$domain.sslscan;
 echo "==========================================================";done
+#iamfaizan
